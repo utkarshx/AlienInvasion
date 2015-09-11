1e6bafc1818ca1504fc152ef6fb90a76155f1f35
Utkarsh Shukla <utk.shukla@gmail.com>
2015-09-11 21:34:29 +0530

    Changed Game

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ var sprites = {
  enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
  enemy_bee: { sx: 79, sy: 0, w: 37, h: 43, frames: 1 },
  enemy_ship: { sx: 116, sy: 0, w: 42, h: 43, frames: 1 },
- enemy_circle: { sx: 158, sy: 0, w: 32, h: 33, frames: 1 },
+ enemy_circle: { sx: 158, sy: 0, w: 52, h: 33, frames: 1 },
  explosion: { sx: 0, sy: 64, w: 64, h: 64, frames: 12 },
  enemy_missile: { sx: 9, sy: 42, w: 3, h: 20, frame: 1, }
 };
@@ -39,8 +39,8 @@ var startGame = function() {
     Game.setBoard(1,new Starfield(50,0.6,100));
     Game.setBoard(2,new Starfield(100,1.0,50));
   }  
-  Game.setBoard(3,new TitleScreen("Alien Invasion", 
-                                  "Press fire to start playing",
+  Game.setBoard(3,new TitleScreen("Need Vs Wants", 
+                                  "Shoot on Wants and Do not Kill Needs",
                                   playGame));
 };
 
